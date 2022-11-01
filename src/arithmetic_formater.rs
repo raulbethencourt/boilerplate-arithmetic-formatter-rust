3d35105dc0fdf0f5f165043b00d44658d042cb21
raBeta <raul.bethencourt.pro@gmail.com>
2022-11-01 20:46:29 +0100

    :construction: start logic

--- src/arithmetic_formater.rs
+++ src/arithmetic_formater.rs
@@ -1,3 +1,30 @@
-pub fn arithmetic_formater<'a>(problems: &'a [&'a str]) -> &'a str {
-    problems[1]
+pub fn arithmetic_formater(problems: Vec<&str>) -> &str {
+    let len_problems = problems.len();
+    println!("{:?}", problems);
+
+    if len_problems > 4 {
+        "Error: Too many problems."
+    } else {
+        for problem in problems {
+            let problem_array: Vec<&str> = problem.split_whitespace().collect();
+            let first_nb: &str = problem_array[0];
+            let second_nb: &str = problem_array[2];
+            let operator: &str = problem_array[1];
+            let len_first = first_nb.len();
+            let len_second = second_nb.len();
+
+            if operator != "+" || operator != "-" {
+                return "Error: Operator must be '+' or '-'.";
+            } else if !first_nb.chars().next().unwrap().is_numeric()
+                || !second_nb.chars().next().unwrap().is_numeric()
+            {
+                return "Error: Numbers must only contain digits.";
+            } else if len_first > 4 || len_second > 4 {
+                return "Error: Numbers cannot be more than four digits.";
+            } else {
+                return "cool";
+            }
+        }
+        return "cool"
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod arithmetic_formater;
 use arithmetic_formater::arithmetic_formater;
 
 fn main() {
-    let problems: &[&str] = &["32 + 698", "3801 - 2", "45 + 43", "123 + 49"];
+    let problems: Vec<&str> = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"].to_vec();
     
     println!("{:?}", arithmetic_formater(problems));
 }
