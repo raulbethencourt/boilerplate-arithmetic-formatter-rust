f95b0cb28503a5b716ebc2d7ec7953062791e5f1
raBeta <raul.bethencourt.pro@gmail.com>
2022-11-14 22:13:25 +0100

    :construction: working hard

--- src/arithmetic_formater.rs
+++ src/arithmetic_formater.rs
@@ -4,35 +4,31 @@ pub fn arithmetic_formater(problems: Vec<&str>) -> String {
     if len_problems > 4 {
         String::from("Error: Too many problems.")
     } else {
-        let mut first_line: String;
-        let mut second_line: String;
-        let mut separator: String;
+        let mut first_line: String = String::from("");
+        let mut second_line: String = String::from("");
+        let mut separator: String = String::from("");
 
-        for problem in problems {
+        for problem in problems.iter() {
             let problem_array: Vec<&str> = problem.split_whitespace().collect();
             let first_nb: String = String::from(problem_array[0]);
             let second_nb: String = String::from(problem_array[2]);
             let operator: String = String::from(problem_array[1]);
 
             let spaces: usize = create_spaces(&first_nb, &second_nb);
-            first_line = format!(
-                "{}    {}",
-                &first_line,
-                create_line2(first_nb, &operator, &spaces)
-            );
+            first_line = format!("{}    {}", first_line, create_line(first_nb, spaces));
         }
 
         first_line
     }
 }
 
-fn create_line2<'a>(number: String, operator: &String, spaces: &usize) -> String {
+fn create_line(number: String, spaces: usize) -> String {
     let mut line: String = number;
-    for space in 0..(spaces - &number.len()) - 1 {
-        line.push_str(" ");
-        line.push_str(line.chars().as_str());
+    for i in 0..(spaces - line.len()) - 1 {
+        line.push(' ');
+        let extra: &str = line.as_str();
+        line.push_str(extra);
     }
-    println!("{}", line);
     line
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod arithmetic_formater;
 use arithmetic_formater::arithmetic_formater;
 
 fn main() {
-    let problems: Vec<&str> = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"].to_vec();
+    let problems: Vec<&str> = vec!["32 + 698", "3801 - 2", "45 + 43", "123 + 49"];
     
     println!("{:?}", arithmetic_formater(problems));
 }
